1c9bc5c60fb488eee48effed7b1ae2985faf049f
Idris Yusuf <yusid03@gmail.com>
2023-09-07 10:07:28 +0100

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
-import Sidebar from './components/Sidebar/Sidebar';
-import Header from './components/Header/Header.jsx';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <Router>
       <Sidebar />
       <Header />
